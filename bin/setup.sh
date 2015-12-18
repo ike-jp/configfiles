a3f3251cf85ed631a86c6fcc09d63a9c19cbe4dd
ike-jp <ike-jp@outlook.com>
2015-12-18 13:35:56 +0000

    symlinkを強制に変更

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,9 +7,9 @@
 #curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
 
 # vimrcのsymlink作成
-ln -s ~/configfiles/vimrc ~/.vimrc
+ln -sf ~/configfiles/vimrc ~/.vimrc
 
 # gitconfigのsymlink作成
-ln -s ~/configfiles/gitconfig ~/.gitconfig
+ln -sf ~/configfiles/gitconfig ~/.gitconfig
 
 
