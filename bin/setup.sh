ddc92942da8764a32fd2f687aafc9bd7e5320754
ike-jp <ike-jp@outlook.com>
2015-12-18 18:41:57 +0900

    added fetch.prume

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+# gitコマンド補完
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
+
+# git管理下のディレクトリでブランチ名を表示する
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
+
 
